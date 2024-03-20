4cc9241d60661bcb6e531cce44a6b7b357a29fa6
Karina <karina.isl@inbox.ru>
2024-03-21 05:08:38 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const button = document.querySelector('#btn');
+button.addEventListener('click', calculateAmount);
+
+const buttonTip = document.querySelector('#addTip');
+buttonTip.addEventListener('click', showTip);
+
+function showTip(e){
+    e.preventDefault();
+    tip.style.display = 'block';
+}
+
+
+function calculateAmount(e){
+    e.preventDefault();
+    const bill = document.querySelector('#bill').value;
+    const people = document.querySelector('#people').value;
+    const tip = document.querySelector('#tip').value;
+
+    if (bill === '' || people === '' || people < 1){
+        Swal.fire({
+            icon: 'error',
+            title: 'Error!',
+            text: 'Введите свои данные!',
+            footer: '<a href="">Why do I have this issue?</a>'
+          });
+    }
+let amountPerPerson = bill/people;
+let tipPerPerson = (bill*tip)/people;
+let totalSum = amountPerPerson+tipPerPerson;
+
+amountPerPerson=amountPerPerson.toFixed(2);
+tipPerPerson=tipPerPerson.toFixed(2);
+totalSum=totalSum.toFixed(2);
+
+document.querySelector('#dividedBill').textContent=amountPerPerson;
+document.querySelector('#dividedTip').textContent=tipPerPerson;
+document.querySelector('#billAndTip').textContent=totalSum;
+
+}
